607077e0e0cb380ebfdde052a05d1d7d830f2bbd
Per Jacobsson <pelle.jacobsson@gmail.com>
2022-10-18 21:14:48 +0200

    2015 day 24

--- 2015/day24/src/main.rs
+++ 2015/day24/src/main.rs
@@ -5,19 +5,73 @@ use std::collections::HashSet;
 
 fn main() {
     let w = read_input("../24_input.txt");
-    let e_min: usize = 113;
-    let e_min = e_min.pow(6);
+    let part1 = calc_entanglement1(w);
+    println!("Part 1: {part1}");
+    let w = read_input("../24_input.txt");
+    let part2 = calc_entanglement2(w);
+    println!("Part 2: {part2}");
+}
+
+fn calc_entanglement1(w: Vec<usize>) -> usize {
+    let mut e_min: usize = 113;
+    e_min = e_min.pow(6);
     for (i, j, k) in [(6, 6, 16), (6, 8, 14), (6, 10, 12), (8, 8, 12), (8, 10, 10)].iter().copied() {
         for p1 in w.iter().copied().combinations(i) {
-            if p1.iter().sum::<usize>() == 516 && p1.iter().product::<usize>() < e_min {
+            if p1.iter().copied().sum::<usize>() == 516 && p1.iter().product::<usize>() < e_min {
                 let ws: HashSet<usize> = w.iter().copied().collect();
-                let p1s: HashSet<usize> = p1.into_iter().collect();
+                let p1s: HashSet<usize> = p1.iter().copied().collect();
                 for p2 in ws.difference(&p1s).copied().combinations(j) {
-                    if p2.iter().sum() == 516 {
+                    if p2.iter().sum::<usize>() == 516 {
                         let p2s: HashSet<usize> = p2.into_iter().collect();
-                        let p3s = ws.difference(&p1s).iter().difference(&p2s);
-                        if p3s.sum() == 516 {
-                            e_min = p1.product();
+                        let p3s = ws.difference(&p1s).collect::<Vec<&usize>>();
+                        let p3s: HashSet<usize> = p3s.into_iter().copied().collect();
+                        let p3s = p3s.difference(&p2s).collect::<Vec<&usize>>();
+                        //.difference(&p2s);
+                        if p3s.into_iter().sum::<usize>() == 516 {
+                            e_min = p1.iter().product();
+                        }
+                    }
+                }
+            }
+        }
+    }
+    e_min
+}
+
+fn calc_entanglement2(w: Vec<usize>) -> usize {
+    let mut e_min: usize = 113;
+    e_min = e_min.pow(5);
+    let mut ss = HashSet::new();
+    for i in [5, 7] {
+        for j in (i..(28 - 3 * i + 1)).step_by(2) {
+            for k in (j..(28 - 2 * i - j + 1)).step_by(2) {
+                let l = 28 - i - j - k;
+                let mut sss = [i, j, k, l];
+                sss.sort();
+                let sss: (usize, usize, usize, usize) = (sss[0], sss[1], sss[2], sss[3]);
+                ss.insert(sss);
+            }
+        }
+    }
+    //let s = ss.iter().copied().collect::<Vec<(usize, usize, usize, usize)>>();
+    for (i, j, k, l) in ss.iter().copied() {
+        for p1 in w.iter().copied().combinations(i) {
+            if p1.iter().copied().sum::<usize>() == 387 && p1.iter().product::<usize>() < e_min {
+                let ws: HashSet<usize> = w.iter().copied().collect();
+                let p1s: HashSet<usize> = p1.iter().copied().collect();
+                for p2 in ws.difference(&p1s).copied().combinations(j) {
+                    if p2.iter().sum::<usize>() == 387 {
+                        let p2s: HashSet<usize> = p2.into_iter().collect();
+                        let p3s = ws.difference(&p1s).collect::<Vec<&usize>>();
+                        let p3s: HashSet<usize> = p3s.into_iter().copied().collect();
+                        let p3s = p3s.difference(&p2s).collect::<Vec<&usize>>();
+                        let p3s: HashSet<usize> = p3s.into_iter().copied().collect();
+                        for p3 in p3s.iter().copied().combinations(k) {
+                            let p4s: HashSet<usize> = p3.into_iter().collect();
+                            let p5s = p3s.difference(&p4s).collect::<Vec<&usize>>();
+                            if p5s.into_iter().sum::<usize>() == 387 {
+                                e_min = p1.iter().product();
+                            }
                         }
                     }
                 }
