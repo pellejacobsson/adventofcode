652f739c6378c75080a6f7eac97aeef0a8d6b2c2
Per Jacobsson <pelle.jacobsson@gmail.com>
2023-12-06 22:28:30 +0100

    2023 day 6

--- 2023/day06.jl
+++ 2023/day06.jl
@@ -1,17 +1,17 @@
-function calc_dist(time, dist)
-    my_dist = [(time - hold_time) * hold_time for hold_time = 1:time-1]
-    sum(my_dist .> dist)
+function calc_dist(t, d)
+    my_d = [(t - t_h) * t_h for t_h = 1:t-1]
+    sum(my_d .> d)
 end
 
 function solve(;part=1)
-    times = [60, 94, 78, 82]
-    dists = [475, 2138, 1015, 1650]
+    ts = [60, 94, 78, 82]
+    ds = [475, 2138, 1015, 1650]
     if part == 1
-        prod(calc_dist.(times, dists))
+        prod(calc_dist.(ts, ds))
     else
-        time = parse(Int, join(string.(times)))
-        dist = parse(Int, join(string.(dists)))
-        calc_dist(time, dist)
+        t = parse(Int, join(string.(ts)))
+        d = parse(Int, join(string.(ds)))
+        calc_dist(t, d)
     end
 end
 
