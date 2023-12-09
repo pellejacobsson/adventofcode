fc9c43dd67760034f2c14cd013d303e23de6091e
Per Jacobsson <pelle.jacobsson@gmail.com>
2023-12-09 08:46:25 +0100

    2023 day 8

--- 2023/day08.jl
+++ 2023/day08.jl
@@ -1,43 +1,56 @@
-using Primes
 using Base.Iterators
 
 function read_input(filename)
-    moves = ""
-    nodes = Dict{String, Tuple{String, String}}()
     open(filename) do f
         moves = readline(f)
+        nodes = Dict{String, Tuple{String, String}}()
         readline(f)
         for line in readlines(f)
             m = match(r"(.+) = \((.+), (.+)\)", line)
             nodes[m[1]] = (m[2], m[3])
         end
+        moves, nodes
     end
-    moves, nodes
 end
 
 function solve(moves, nodes)
     node = "AAA"
     for (i, move) in enumerate(cycle(moves))
         node = move == 'L' ? nodes[node][1] : nodes[node][2]
-        if node == "ZZZ"
-            return i
+        node == "ZZZ" && return i
+    end
+end
+
+function check_cyclic(moves, nodes, node)
+    cycle_l = Int[]
+    cycle_n = 1
+    for (i, move) in enumerate(cycle(moves))
+        node = move == 'L' ? nodes[node][1] : nodes[node][2]
+        if endswith(node, "Z")
+            push!(cycle_l, i)
+            cycle_n > 1 ? break : cycle_n += 1
         end
     end
+    return cycle_l[2] / cycle_l[1] == 2
+end
+
+function check_all_cyclic(moves, nodes)
+    all(check_cyclic(moves, nodes, node) for node in keys(nodes) if endswith(node, "A"))
 end
 
 function solve2(moves, nodes)
     curr_nodes = [node for node in keys(nodes) if endswith(node, "A")]
-    factors = Dict{Int, Int}()
+    cycle_l = Int[]
     for node in curr_nodes
         for (i, move) in enumerate(cycle(moves))
             node = move == 'L' ? nodes[node][1] : nodes[node][2]
             if endswith(node, "Z")
-                factors = mergewith(max, factors, factor(Dict, i))
+                push!(cycle_l, i)
                 break
             end
         end
     end
-    prod([f * m for (f, m) in factors])
+    lcm(cycle_l)
 end
 
 function runit(part=1)
