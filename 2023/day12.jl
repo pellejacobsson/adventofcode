0d47d0c40e2c580660b2cd40b54ffe50c71f3878
Per Jacobsson <per.jacobsson@consultant.volvo.com>
2023-12-13 13:56:11 +0100

    2023 day 12

---
+++ 2023/day12.jl
@@ -0,0 +1,61 @@
+using IterTools
+
+function read_input(filename, part2=false)
+    records = []
+    groups = []
+    rec_map = Dict('.' => 0, '#' => 1, '?' => 2)
+    open(filename) do f
+        for line in readlines(f)
+            r, g = split(line)
+            if part2
+                r = join(repeat(r, 5), "?")
+            end
+            push!(records, [rec_map[s] for s in r])
+            push!(groups, parse.(Int, split(g, ",")))
+        end
+    end
+    if part2
+        groups = [repeat(g, 5) for g in groups]
+    end
+    records, groups
+end
+
+function get_record_perms(record, group)
+    ix = findall(x -> x == 2, record)
+    ix1 = findall(x -> x == 1, record)
+    nix = length(ix)
+    perms = [digits(n, base=2, pad=nix) for n = 0:2^nix - 1]
+    perms = [perm for perm in perms if sum(perm) + sum(record[ix1])== sum(group)]
+    precords = Vector{Int64}[]
+    for perm in perms
+        precord = copy(record)
+        precord[ix] = perm
+        push!(precords, precord)
+    end
+    precords
+end
+
+function count_record_perm(record, group)
+    precords = get_record_perms(record, group)
+    s = 0
+    for precord in precords
+        isfit = [length(g) for g in groupby(x -> x, precord) if g[1] == 1] == group
+        s += isfit ? 1 : 0
+    end
+    s
+end
+
+function solve(records, groups)
+    n = 0
+    for (record, group) in zip(records, groups)
+        n += count_record_perm(record, group)
+    end
+    n
+end
+
+function runit(filename; part2=false)
+    records, groups = read_input(filename, part2)
+    solve(records, groups)
+end
+
+runit("12_input.txt")
